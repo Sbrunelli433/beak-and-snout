188064b4acb02784ca61eaf72fcc69da2ed8804f
Steve Brunelli <brunes46@gmail.com>
2024-01-05 15:08:19 -0600

    replaced content with lorem ipsum text

--- beak-and-snout-apothecary/src/routes/about/about.component.jsx
+++ beak-and-snout-apothecary/src/routes/about/about.component.jsx
@@ -6,11 +6,14 @@ const About = () => {
         <div className="about-us-hero"></div>
         <div className="about-us-content">
           <h2>about us!</h2>
-
           <p>
-            Meet Anna and Ben! We are the founders of Beak and Snout. While Anna
-            makes the soap, Ben is a master class Destiny 2 player and carries
-            his entire clan through every Dungeon and Raid.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
